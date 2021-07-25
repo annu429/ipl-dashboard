bcc7ae374545bee565fe1b12ba9788cff3b1a6a9
annu429 <anandparmar429@gmail.com>
2021-07-25 09:24:06 +0530

    Added MatchPage with TeamController and MatchRepository.java

--- src/main/java/io/javalearn/ipldashboard/controller/TeamController.java
+++ src/main/java/io/javalearn/ipldashboard/controller/TeamController.java
@@ -1,10 +1,15 @@
 package io.javalearn.ipldashboard.controller;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.javalearn.ipldashboard.model.Match;
 import io.javalearn.ipldashboard.model.Team;
 import io.javalearn.ipldashboard.repository.MatchRepository;
 import io.javalearn.ipldashboard.repository.TeamRepository;
@@ -27,5 +32,14 @@ public class TeamController {
         team.setMatches(this.matchRepository.findLatestMatchesbyTeam(teamName, 4));
 
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName,@RequestParam int year) {
+        /**Here taking two dates for in between date we will get Match data  */
+        LocalDate startDate = LocalDate.of(year, 1, 1); // (year,jan, 1st date of jan)
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+       return this.matchRepository.getMatchesByTeamBetweenDates(teamName, startDate, endDate);
+        
     }
 }
