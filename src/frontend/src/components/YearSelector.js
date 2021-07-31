7c6b9fa639133c722791c7eb01573416c1c3e2b2
annu429 <anandparmar429@gmail.com>
2021-07-31 13:44:13 +0530

    Done IPL Dashboard

--- src/frontend/src/components/YearSelector.js
+++ src/frontend/src/components/YearSelector.js
@@ -1,4 +1,4 @@
-import {React, useEffect, useState} from 'react';
+import {React} from 'react';
 import { Link } from 'react-router-dom';
 
 import './YearSelector.scss';
@@ -15,8 +15,8 @@ export const YearSelector = ({teamName}) => {
     return(
         <ol className="YearSelector">
         {years.map(year =>( 
-            <li>
-                <Link to={`/teams/${teamName}/matches/${year}`}>{year}</Link>
+            <li key={year}>
+                <Link  to={`/teams/${teamName}/matches/${year}`}>{year}</Link>
             </li>)
         
         )}
